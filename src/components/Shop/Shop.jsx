1d2ad63f7bed6cdf5abbd9efea1e9d87b3510df1
Mirza Mohibul Hasan <mirzamohibul618@gmail.com>
2023-03-31 06:31:52 +0600

    retrive product from stored cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
@@ -9,11 +11,46 @@ const Shop = () => {
         .then( data => setProducts(data))
     }, [])
 
+    /* useEffect(()=>{
+        const storedCart = getShoppingCart();
+        // Step-01: get id
+        for(const id in storedCart){
+            // step -2: get product by id
+            const addedProduct = products.find(product => product.id === id)
+
+            // Step-03: get the quantity of product
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            console.log(addedProduct)
+        }
+    },[products]) */
+
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step-01: get id
+        for(const id in storedCart){
+            // Step-02: get products from products state using id
+            const addedProducts = products.find(product => product.id === id)
+            if(addedProducts){
+                // Step-03: Add quantity
+                const quantity = storedCart[id];
+                addedProducts.quantity = quantity;
+
+                //Step-4:Add the added product to the saved cart
+                savedCart.push(addedProducts);
+            }
+
+        }
+        // Step-05: Set the cart
+        setCart(savedCart);
+    },[products])
 
     const [cart, setCart] = useState([]);
     const handleAddToCart = (product) =>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
@@ -28,8 +65,7 @@ const Shop = () => {
                 }
             </div>
             <div className='cart-container'>
-                <h4>Order Summary</h4>
-                <p>Number of Orders: {cart.length}</p>
+                <Cart cart ={cart}></Cart>
             </div>
             
         </div>
